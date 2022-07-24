38efa396c9bd571a82c3071ba614103f4bcc2a06
Shivank Swami <69349438+Shivankswami77@users.noreply.github.com>
2022-07-24 10:07:11 +0530

    Commit

--- src/components/routes/sign-in/sign-in.component.jsx
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 import {
+  auth,
   createUserDocumentFromAuth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
 } from "../../../utils/firebase.utils";
 
 const SignIn = () => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(async () => {
+    const response = await getRedirectResult(auth);
+    if (response) {
+      const userDocRef = await createUserDocumentFromAuth(response.user);
+    }
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
+
+    console.log(user);
   };
+
   return (
     <div>
       SignIn
       <button onClick={logGoogleUser}>Sign in with google</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with google redirect
+      </button>
     </div>
   );
 };
